328300616bc93973a8e32357459661c5d68c7b28
lnr0208 <absqwerty3@gmail.com>
2023-01-11 19:23:38 +0500

    created login screen

--- app/src/main/java/com/isayevapps/projectbylectures_easycoderu/MainActivity.kt
+++ app/src/main/java/com/isayevapps/projectbylectures_easycoderu/MainActivity.kt
@@ -1,60 +1,46 @@
 package com.isayevapps.projectbylectures_easycoderu
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.view.View
-import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val agreementTextView: TextView = findViewById(R.id.agreementTextView)
-
-        val fullText = getString(R.string.agreement_fulltext)
-        val confidential = getString(R.string.confidential_info)
-        val policy = getString(R.string.privacy_policy)
-        val spannableString = SpannableString(fullText)
-        val confidentialSpan = MyClickableSpan {
-            Snackbar.make(it, "Go to Link 1", Snackbar.LENGTH_SHORT).show()
-        }
-        val policySpan = MyClickableSpan {
-            Snackbar.make(it, "Go to Link 2", Snackbar.LENGTH_SHORT).show()
-        }
-
-        spannableString.setSpan(
-            confidentialSpan,
-            fullText.indexOf(confidential),
-            fullText.indexOf(confidential) + confidential.length,
-            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        spannableString.setSpan(
-            policySpan,
-            fullText.indexOf(policy),
-            fullText.indexOf(policy) + policy.length,
-            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        agreementTextView.run {
-            text = spannableString
-            movementMethod = LinkMovementMethod.getInstance()
-            highlightColor = Color.BLUE
-        }
+        val textInputLayout = findViewById<TextInputLayout>(R.id.emailInputLayout)
+        val editTextView = textInputLayout.editText as TextInputEditText
+        editTextView.addTextChangedListener(object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                val isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
+                textInputLayout.isErrorEnabled = !isValid
+                val error = if (isValid) "" else getString(R.string.invalid_email)
+                textInputLayout.error = error
+                if (isValid) Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.valid_email),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
 
-class MyClickableSpan(private val lambda: (view: View) -> Unit) : ClickableSpan() {
-    override fun onClick(widget: View) = lambda.invoke(widget)
-    override fun updateDrawState(ds: TextPaint) {
-        super.updateDrawState(ds)
-        ds.isUnderlineText = true
-        ds.color = Color.rgb(255, 0, 0)
+
+abstract class SimpleTextWatcher : TextWatcher {
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
     }
 }
